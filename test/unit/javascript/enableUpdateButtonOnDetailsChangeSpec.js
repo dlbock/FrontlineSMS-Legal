a2ca3add332fc6a0fb191f3a66026e99ba3516ba
Richa Agrawal <richa.agrawal89@gmail.com>
2011-08-26 16:12:50 +0530

    Richa Pratima removedcommented code in enableUpdateButtonOnDetailsChangeSpec

--- test/unit/javascript/enableUpdateButtonOnDetailsChangeSpec.js
+++ test/unit/javascript/enableUpdateButtonOnDetailsChangeSpec.js
@@ -14,42 +14,8 @@ describe('enableUpdateButtonOnDetailsChange', function () {
         expect($("#update").attr("disabled")).toEqual("disabled");
     });
 
-
-//    it('should be enabled when text box data is changed', function () {
-//
-//        frontlinesms.enableUpdateButtonOnDetailsChangeForKeyDown("#text-description","#update");
-//        expect($("#update").attr("disabled")).toBeUndefined();
-//    });
-
-
     afterEach(function(){
         $('body#fixtures > *').not(".jasmine_reporter").not('script').remove();
     });
 });
-
-//    frontlinesms.enableUpdateButtonOnDetailsChangeForKeyDown = function(textboxId, buttonId){
-//
-//        var press = jQuery.Event("keypress");
-//        press.which = 40;
-//        $(textboxId).trigger(press);
-//        alert($(textboxId).val());
-
-
-//        var keyup = $.Event('keypress');
-//        alert($.ui.keyCode.Z);
-//        var content = $(textboxId).val();
-//        $(textboxId).focus();
-//        $(textboxId).trigger(keyup);
-//        alert($(textboxId).val());
-//
-//        $(textboxId).keypress(function(){
-//            alert($(textboxId).val());
-//
-//            if ($(textboxId).val() != content){
-//                $(buttonId).removeAttr("disabled");
-//            }
-//        });
-
-//    }
-//});
 
