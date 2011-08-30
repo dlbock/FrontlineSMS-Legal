6881ac48483debf89771802f5f5d5019fdae254c
Ben Barnard <barnardb@gmail.com>
2011-08-30 08:47:56 +0530

    Ben: minor refactoring of UpdateCore to remove repetition

--- scripts/UpdateCore.groovy
+++ scripts/UpdateCore.groovy
@@ -2,17 +2,13 @@ includeTargets << grailsScript("_GrailsProxy")
 includeTargets << grailsScript("_GrailsPlugins")
 
 target(updateCore: "Update FrontlineSMS Core plugin") {
-    if (connectionAvailable(new URL("http://221.134.198.6/job/FrontlineSMS2-Core/lastSuccessfulBuild/artifact/grails-frontlinesms2-0.1-SNAPSHOT.zip"))){
-        def url = "http://221.134.198.6/job/FrontlineSMS2-Core/lastSuccessfulBuild/artifact/grails-frontlinesms2-0.1-SNAPSHOT.zip"
-    args = url
+    def hostNames = ["221.134.198.6", "localhost"]
+    def urlsToTry = hostNames.collect {"http://${it}/job/FrontlineSMS2-Core/lastSuccessfulBuild/artifact/grails-frontlinesms2-0.1-SNAPSHOT.zip"}
+    def url = urlsToTry.find {connectionAvailable(new URL(it))}
+    if (url) {
+        args = url
         installPlugin()
-    }
-    else if (connectionAvailable(new URL("http://localhost/job/FrontlineSMS2-Core/lastSuccessfulBuild/artifact/grails-frontlinesms2-0.1-SNAPSHOT.zip"))){
-        def url = "http://localhost/job/FrontlineSMS2-Core/lastSuccessfulBuild/artifact/grails-frontlinesms2-0.1-SNAPSHOT.zip"
-           args = url
-                installPlugin()
-    }
-    else {
+    } else {
         echo "WARNING: Could not load the latest version of FrontlineSMS Core"
     }
 }
