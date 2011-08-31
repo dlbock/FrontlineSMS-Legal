59ee3418b45322ffd333b8ff8817cf04e3c44a95
dahlia <dlbock@gmail.com>
2011-08-31 13:35:47 +0530

    dahlia: adding timing to the precheckin script to print out build time in seconds

--- scripts/precheckin.sh
+++ scripts/precheckin.sh
@@ -4,12 +4,16 @@ set -e
 if [ -d $GRAILS_HOME ]
 then
     echo "Using Grails in $GRAILS_HOME"
+    before="$(date +%s)"
     grails clean
     grails update-core
     grails codenarc
     grails sass-compile
     grails test-app -coverage
     grails dist
+    after="$(date +%s)"
+    elapsed_time="$(expr $after - $before)"
+    echo "Build took $elapsed_time secs."
 else
     echo "GRAILS_HOME is not set."
 fi
