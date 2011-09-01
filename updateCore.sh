b4105e4c9551cfd4ddbed815d0f489de84c4fdfa
Ben Barnard <barnardb@gmail.com>
2011-09-01 11:30:45 +0530

    Ben/Richa: use sed instead of bash variable substitution to perform percent encoding because the latter doesn't work in other shells

--- updateCore.sh
+++ updateCore.sh
@@ -5,8 +5,8 @@ jenkins_server=221.134.198.6
 project=FrontlineSMS2-Core
 
 metadata_url=http://$jenkins_server/job/$project/lastStableBuild/api/json?tree=actions[lastBuiltRevision[SHA1]]
-metadata_url=${metadata_url//[/%5B}
-metadata_url=${metadata_url//]/%5D}
+metadata_url=$(echo "$metadata_url" | sed -E "s/\[/%5B/g")
+metadata_url=$(echo "$metadata_url" | sed -E "s/\]/%5D/g")
 
 metadata=$(curl --silent "$metadata_url")
 last_successful_revision=$(echo "$metadata" | sed -E 's/.*"SHA1":"([0-9a-f]{40}).*/\1/')
