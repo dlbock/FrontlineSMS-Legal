99a007775f75dbc4ec3ee052ce68d8e8c5b8adc7
Aditi Raveesh <aditi.raveesh@gmail.com>
2011-09-02 14:50:04 +0530

    Aditi/Sneha: Legal-59: Enabled delete, backspace and other valid keys in contact number field in the create contact page

--- src/javascript/formValidation.js
+++ src/javascript/formValidation.js
@@ -2,10 +2,16 @@ var frontlinesms = this.frontlinesms || {};
 
 frontlinesms.validateContactNumber = function() {
     var validCharacters = ['0','1','2','3','4','5','6','7','8','9','#','+','-','*'];
+    var keyCodesForAllowedKeys = [8, 9, 37, 38, 39, 40, 46];
     $("#contact-primary-mobile").keypress(function(key) {
             var characterPressed = String.fromCharCode(key.charCode);
-    
             var i;
+            for (i = 0; i < keyCodesForAllowedKeys.length; i++) {
+                if(key.keyCode == keyCodesForAllowedKeys[i]) {
+                    return true;
+                }
+            }
+
             for (i = 0; i < validCharacters.length; i++) {
                 if (characterPressed == validCharacters[i]) {
                     return true;
