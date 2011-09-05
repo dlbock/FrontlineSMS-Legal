0c8a44d0cfaa941a18e0d43e20edd8d526692256
Neetu Mishra <neetumis@thoughtworks.com>
2011-09-05 20:04:57 +0530

    Neetu/Saumya: primary mobile number accept only number & some special character if the user pastes any string containing alphanumeric character

--- src/javascript/formValidation.js
+++ src/javascript/formValidation.js
@@ -3,6 +3,27 @@ var frontlinesms = this.frontlinesms || {};
 frontlinesms.validateContactNumber = function() {
     var validCharacters = ['0','1','2','3','4','5','6','7','8','9','#','+','-','*'];
     var keyCodesForAllowedKeys = [0, 8, 9];
+
+
+    $("#contact-primary-mobile").change(function(){
+        var validMobileNumber= "";
+        var primaryMobileNumber = $(this).val();
+           for(var i=0; i < primaryMobileNumber.length ; i++)
+           {
+               for(var j=0 ; j < validCharacters.length ; j++){
+                   if(primaryMobileNumber[i] == validCharacters[j]){
+                       validMobileNumber = validMobileNumber + primaryMobileNumber[i];
+                   }
+               }
+               for(var k=0 ; k < keyCodesForAllowedKeys.length ; k++){
+                   if(primaryMobileNumber[i] == keyCodesForAllowedKeys[k]){
+                       validMobileNumber = validMobileNumber + primaryMobileNumber[i];
+                   }
+               }
+           }
+        $("#contact-primary-mobile").val(validMobileNumber);
+    });
+
     $("#contact-primary-mobile").keypress(function(key) {
             frontlinesms.log(key);
             var characterPressed = String.fromCharCode(key.which);
@@ -12,7 +33,6 @@ frontlinesms.validateContactNumber = function() {
                     return true;
                 }
             }
-
             for (i = 0; i < validCharacters.length; i++) {
                 if (characterPressed == validCharacters[i]) {
                     return true;
@@ -21,17 +41,14 @@ frontlinesms.validateContactNumber = function() {
             return false;
         }
     );
+
 };
 
 frontlinesms.blockKeyPressInDateField = function() {
 
     var keyCodeForTabKey = 9;
     $("#event-date").keypress(function(key) {
-            /*if(key.keyCode == keyCodeForTabKey)
-            {
-                return true;
-            }*/
-            return false;
-        }
+           return false;
+    }
     );
 };
---
+++ test/unit/javascript/formValidationSpec.js
@@ -0,0 +1,20 @@
+describe("form validation", function() {
+    beforeEach(function() {
+        var tempHTML =
+                '<input id="contact-primary-mobile" type="text" value="8675309FFF+!*-%$#"/>';
+        $(tempHTML).appendTo("#fixtures");
+        frontlinesms.validateContactNumber();
+    });
+
+    it("accept only number & some special character if the user pastes any string containing alphanumeric character ", function() {
+        $("#contact-primary-mobile").val("hello123+*-#$/")
+        $("#contact-primary-mobile").change();
+        expect($("#contact-primary-mobile").val()).toEqual("123+*-#");
+    });
+
+    afterEach(function() {
+        $("#contact-primary-mobile").remove();
+    });
+
+});
+
