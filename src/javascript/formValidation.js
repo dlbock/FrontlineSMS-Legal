94de2cfd57dd59f8441370a860bf6e2a1a198eea
Neetu Mishra <neetuangel.mishra@gmail.com>
2011-09-06 11:59:02 +0530

    Saumya & Neetu: Legal 81 : a copy paste action filters the unwanted/not allowed characters from the contact number field - works in firefox also now

--- src/javascript/formValidation.js
+++ src/javascript/formValidation.js
@@ -2,20 +2,22 @@ var frontlinesms = this.frontlinesms || {};
 
 frontlinesms.validateContactNumber = function() {
     var validCharacters = ['0','1','2','3','4','5','6','7','8','9','#','+','-','*'];
-    var keyCodesForAllowedKeys = [0, 8, 9];
+    var keyCodesForAllowedKeys = [0, 8, 9, 118];
+    var lengthOfContactNumber = 0;
 
     $("#contact-primary-mobile").keyup(function(){
         var validMobileNumber= "";
         var primaryMobileNumber = $("#contact-primary-mobile").val();
         for(var i=0; i < primaryMobileNumber.length ; i++)
            {
+               var key = primaryMobileNumber.which;
                for(var j=0 ; j < validCharacters.length ; j++){
                    if(primaryMobileNumber[i] == validCharacters[j]){
                        validMobileNumber = validMobileNumber + primaryMobileNumber[i];
                    }
                }
                for(var k=0 ; k < keyCodesForAllowedKeys.length ; k++){
-                   if(primaryMobileNumber[i].which == keyCodesForAllowedKeys[k]){
+                   if(key == keyCodesForAllowedKeys[k]){
                        validMobileNumber = validMobileNumber + primaryMobileNumber[i];
                    }
                }
@@ -25,10 +27,14 @@ frontlinesms.validateContactNumber = function() {
 
     $("#contact-primary-mobile").keypress(function(key) {
             frontlinesms.log(key);
-            var characterPressed = String.fromCharCode(key.which);
+            var keypressed = key? key.which : window.event.keyCode;
+            var characterPressed = String.fromCharCode(keypressed);
+            lengthOfContactNumber++;
+            if(lengthOfContactNumber < 26 || keypressed!=8 || keypressed!=0 || keypressed!=9)
+            {
             var i;
             for (i = 0; i < keyCodesForAllowedKeys.length; i++) {
-                if (key.keyCode == keyCodesForAllowedKeys[i]) {
+                if(keypressed == keyCodesForAllowedKeys[i]) {
                     return true;
                 }
             }
@@ -36,6 +42,7 @@ frontlinesms.validateContactNumber = function() {
                 if (characterPressed == validCharacters[i]) {
                     return true;
                 }
+            }
             }
             return false;
         }
