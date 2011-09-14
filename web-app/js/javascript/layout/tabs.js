89d8fbde3f93ff22ad87dbcd5f3f9fe8a1479a98
dahlia <dlbock@gmail.com>
2011-09-14 07:17:57 +0530

    dahlia: removing generated js files that shouldn't have been checked in

--- web-app/js/javascript/layout/tabs.js
+++
@@ -1,8 +0,0 @@
-var frontlinesms = this.frontlinesms || {};
-
-frontlinesms.tabs = function () {
-    var selectedTab = $("#active-tab").attr('class');
-    if (selectedTab) {
-        $(".tab." + selectedTab).addClass("selected");
-    }
-};
