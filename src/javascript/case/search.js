73af6624c1548534f4d70bacb6b2ffc4e28168c5
Elisa Cutrin <ecutrina@thoughtworks.com>
2011-09-15 16:00:00 +0530

    Rupa/Elisa: Cleaning JS and removing duplications: Removed caseSearchForSearchPage.js that was not being used anymore

--- src/javascript/case/search.js
+++ src/javascript/case/search.js
@@ -1,19 +1,22 @@
 var frontlinesms = this.frontlinesms || {};
 
 frontlinesms.caseSearchOnLoad = function() {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+
     frontlinesms.initializeAutocomplete();
 }
 
 frontlinesms.initializeAutocomplete = function() {
-    //$("#SearchResults tbody tr").attr('filtermatch','true')
-    //$("#SearchResults tbody tr").attr('style','display:true')
     $("#SearchResults").tableFilter({
         additionalFilterTriggers: [$("#caseId")]
     });
-    $("#caseId").removeAttr("title").val("").focus();
     $("#SearchResults thead tr.filters").remove();
+    $("#caseId").removeAttr("title");
 }
-
-
-
 
